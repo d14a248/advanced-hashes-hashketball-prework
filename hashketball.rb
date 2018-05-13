0de823b05784360b1f3e7b26e1c1f186bd7378cb
david landau <thelandaus@gmail.com>
2018-05-13 14:59:32 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -227,7 +227,6 @@ def big_shoe_rebounds #solving with array
   num_rebounds = 0
   game_hash.each do  |location,team_data|
     team_data[:players].each do |player,player_data|
-      #binding.pry
       if max_shoe == player_data[:shoe]
         num_rebounds = player_data[:rebounds]
       end
